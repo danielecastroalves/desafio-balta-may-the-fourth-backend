c0f3df203b3e14d6f41e5a964121d59b24da4a6b
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2024-04-26 23:50:52 -0300

    Criação do mapster

--- src/MayTheFourth.Application/Features/Starships/GetStarshipsResponse.cs
+++ src/MayTheFourth.Application/Features/Starships/GetStarshipsResponse.cs
@@ -10,10 +10,10 @@
         public double MaxSpeed { get; set; }
         public int Crew { get; set; }
         public int Passengers { get; set; }
-        public double cargoCapacity { get; set; }
+        public double CargoCapacity { get; set; }
         public float HyperdriveRating { get; set; }
-        public int Mglt { get; set; }
-        public TimeSpan Consumables { get; set; }
+        public int MGTL { get; set; }
+        public string Consumables { get; set; } = null!;
         public string Class { get; set; } = null!;
         public List<ItemDescription>? Movies { get; set; }
     }
