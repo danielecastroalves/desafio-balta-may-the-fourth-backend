87ac8f47361cf1bea2b42b0e532fa8c4d1c60955
JoseCarlosDaSilva <jcds@jcds.net>
2024-04-28 22:53:48 -0300

    Primeira versão do Endpoint de Planetas

---
+++ src/MayTheFourth.Application/Features/Planets/GetPlanetRequest/GetPlanetRequestHandler.cs
@@ -0,0 +1,50 @@
+using Mapster;
+using MayTheFourth.Application.Common.AppServices.PopulateResponseList;
+using MayTheFourth.Application.Common.Repositories;
+using MayTheFourth.Application.Features.Planets;
+using MayTheFourth.Application.Planets.GetPlanetById.GetPlanet;
+using MayTheFourth.Domain.Entities;
+using MediatR;
+
+namespace MayTheFourth.Application.Features.Vehicles.GetVehicle
+{
+    public class GetPlanetRequestHandler : IRequestHandler<GetPlanetRequest, List<GetPlanetResponse>>
+    {
+        private readonly IRepository<FilmEntity> _filmRepository;
+        private readonly IRepository<PlanetEntity> _planetRepository;
+        private readonly IPopulateResponseListAppService _populateResponseListAppService;
+
+        public GetPlanetRequestHandler
+        (
+            IRepository<FilmEntity> filmRepository,
+            IRepository<PlanetEntity> planetRepository,
+            IPopulateResponseListAppService populateResponseListAppService
+        )
+        {
+            _filmRepository = filmRepository;
+            _planetRepository = planetRepository;
+            _populateResponseListAppService = populateResponseListAppService;
+        }
+
+        public async Task<List<GetPlanetResponse>> Handle(GetPlanetRequest request, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var planetList = await _planetRepository.GetListByFilterAsync(x => x.Active);
+            var filmList = await _filmRepository.GetListByFilterAsync(x => x.Active);
+
+            var responseList = new List<GetPlanetResponse>();
+
+            foreach (var planet in planetList)
+            {
+                var response = planet.Adapt<GetPlanetResponse>();
+
+                // TODO: Populate films.
+                // response.Films = _populateResponseListAppService.GetFilmsList(planet.Films, filmList);
+                responseList.Add(response);
+            }
+
+            return responseList;
+        }
+    }
+}
--- src/MayTheFourth.WebApi/Endpoints/PlanetsEndpoints.cs
+++ src/MayTheFourth.WebApi/Endpoints/PlanetsEndpoints.cs
@@ -1,3 +1,11 @@
+using MayTheFourth.Application.Features.Films;
+using MayTheFourth.Application.Features.Films.GetFilmes;
+using MayTheFourth.Application.Features.Planets;
+using MayTheFourth.Application.Features.Planets.GetPlanetById;
+using MayTheFourth.Application.Planets.GetPlanetById.GetPlanet;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
 namespace MayTheFourth.WebApi.Endpoints
 {
     public static class PlanetsEndpoints
@@ -5,9 +13,46 @@ namespace MayTheFourth.WebApi.Endpoints
         //TODO - Adicionar Endpoints
         public static void MapPlanetsEndpoints(this WebApplication app)
         {
+            var root = app.MapGroup("/api/planetas")
+                .WithTags("Planetas")
+                .WithOpenApi();
 
+            root.MapGet("/", GetPlanetasAsync)
+                .Produces<List<GetPlanetResponse>>()
+                .WithSummary("Obtem todos os planetas cadastrados")
+                .WithDescription("Endpoint para leitura e retorno da lista de planetas cadastrados");
+
+            root.MapGet("/{id}", GetPlanetasByIdAsync)
+                .Produces<GetPlanetResponse>()
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Obtem um planeta cadastrado")
+                .WithDescription("Endpoint para leitura e retorno de um filme cadastrado pelo Id correspondente");
         }
 
         //TODO - Adicionar métodos
+        private static async Task<IResult> GetPlanetasAsync
+        (
+            [FromServices] IMediator mediator,
+            CancellationToken cancellationToken
+        )
+        {
+            var result = await mediator.Send(new GetPlanetRequest(), cancellationToken);
+
+            return Results.Ok(result);
+        }
+
+        private static async Task<IResult> GetPlanetasByIdAsync
+        (
+           [FromRoute] int id,
+           [FromServices] IMediator mediator
+        )
+        {
+            var result = await mediator.Send(new GetPlanetByIdRequest(id));
+
+            if (result is null)
+                return Results.NotFound();
+
+            return Results.Ok(result);
+        }
     }
 }
